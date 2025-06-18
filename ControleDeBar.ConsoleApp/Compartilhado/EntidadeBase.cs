2071c7892f5e273d58fee3ab319bf72c3ef39c41
Calebe <calebepfinger@gmail.com>
2025-06-17 21:32:05 -0300

    Add Project Files

--- ControleDeBar.ConsoleApp/Compartilhado/EntidadeBase.cs
+++ ControleDeBar.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -2,5 +2,9 @@
 {
     public abstract class EntidadeBase
     {
+        public int Id { get; set; }
+
+        public abstract void AtualizarRegistro(EntidadeBase registroAtualizado);
+        public abstract string Validar();
     }
 }
